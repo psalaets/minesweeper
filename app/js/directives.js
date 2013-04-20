7f38623a26deea31fee8d2a367a2d61fe5c2839b
Paul Salaets <psalaets@gmail.com>
2013-04-20 18:58:41 -0400

    bind to right clicks

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,4 +1,15 @@
 'use strict';
 
-/* Directives */
-
+// From http://stackoverflow.com/a/15732476
+angular.module('ms.directives', []).
+  directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+      var fn = $parse(attrs.ngRightClick);
+      element.bind('contextmenu', function(event) {
+        scope.$apply(function() {
+          event.preventDefault();
+          fn(scope, {$event:event});
+        });
+      });
+    }
+  });
