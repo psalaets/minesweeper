16fefc2df3368fc35e83aef3f34a8c97e05bffaf
Paul Salaets <psalaets@gmail.com>
2013-04-20 17:33:24 -0400

    clearing out some auto gen'd stuff I don't use right now

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,17 +3,7 @@
 /* jasmine specs for filters go here */
 
 describe('filter', function() {
-  beforeEach(module('myApp.filters'));
+  beforeEach(module('ms.filters'));
 
 
-  describe('interpolate', function() {
-    beforeEach(module(function($provide) {
-      $provide.value('version', 'TEST_VER');
-    }));
-
-
-    it('should replace VERSION', inject(function(interpolateFilter) {
-      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
-    }));
-  });
 });
